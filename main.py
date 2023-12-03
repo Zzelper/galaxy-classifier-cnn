99b35d0542f8120984e8749cfe90c927ae9ec5f0
Patrick-Thompson9 <146134872+Patrick-Thompson9@users.noreply.github.com>
2023-12-03 16:53:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,74 @@
 from src.data_import import *
-from src.model import *
 import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
 
+class CNN(nn.Module):
+    def __init__(self, num_classes):
+        super(CNN, self).__init__()
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        # Max pooling layer
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        # Fully connected layers
+        self.fc1 = nn.Linear(128 * 32 * 32, 512)  # Adjusted the input size here
+        self.fc2 = nn.Linear(512, num_classes)
+        # Dropout
+        self.dropout = nn.Dropout(0.5)
+        # Activation function
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.pool(x)
+        x = self.relu(self.conv2(x))
+        x = self.pool(x)
+        x = self.relu(self.conv3(x))
+        x = self.pool(x)
+        # Check the size after convolution and pooling
+        # print(x.size())  # Uncomment to print the size for debugging
+        x = x.view(-1, 128 * 32 * 32)  # Adjusted the view size here
+        x = self.dropout(x)
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+        
 if __name__ == "__main__":
-    #print("Hello World")
-    images, labels = load_images_labels(5000)
-    train_images, train_labels, test_images, test_labels = split_dataset(images, labels)
-    #print(images[0])
-    #print(test_images)
-    # Load your dataset and apply data augmentation
-    train_dataset = GalaxyDataset(train_images, train_labels)
-    test_dataset = GalaxyDataset(test_images, test_labels)
+    images, labels = load_images_labels()
+    images = images.permute(0, 3, 1, 2)
+    train_loader = zip(images, labels)
 
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+    # Instantiate the model
+    num_classes = 10  # Adjusted for 10 classes
+    num_epochs = 5  # Define num_epochs
+    model = CNN(num_classes)
 
-    # Initialize the model
-    model = GalaxyCNN(num_classes=10)
-
-    # Define the loss function and optimizer
+    # Define loss function and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-    # Training loop
-    num_epochs = 10
-    for epoch in range(num_epochs):
-        for inputs, labels in train_loader:
+    # This part can be in another script where you load data, preprocess, and train the model.
+    # For brevity, here's a placeholder for training the model using your dataset:
+
+    # Training loop (you need to replace this with your actual data loading and training process)
+    for epoch in range(num_epochs):  # Define num_epochs
+        for images, labels in train_loader:
             optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
+            outputs = model(images)
+            
+            # Ensure the shapes of outputs and labels match
+            # Resize labels to match the batch size of the outputs
+            labels_resized = labels[:outputs.shape[0]].long()  # Convert labels to torch.long
+            
+            loss = criterion(outputs, labels_resized)
             loss.backward()
             optimizer.step()
-        print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item():.4f}")
 
-    # Testing loop
-    model.eval()
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for inputs, labels in test_loader:
-            outputs = model(inputs)
-            _, predicted = torch.max(outputs, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}")
 
-    accuracy = 100 * correct / total
-    print(f"Test Accuracy: {accuracy:.2f}%")
+    # Save the trained model (optional)
+    torch.save(model.state_dict(), 'trained_model.pth')
+
