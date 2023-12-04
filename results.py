244e142bd33f9b44c03b70e94297f5c78f266b4d
Patrick-Thompson9 <146134872+Patrick-Thompson9@users.noreply.github.com>
2023-12-04 15:53:13 -0500

    Create results.py, this tests model and creates confusion matrix

---
+++ results.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+from main import ComplexCNN
+
+def plot_confusion_matrix(df_confusion, title='Confusion matrix', cmap=plt.cm.gray_r):
+
+    plt.matshow(df_confusion, cmap=cmap) # imshow
+    plt.title(title)
+    plt.colorbar()
+    #tick_marks = np.arange(len(df_confusion.columns))
+    #plt.xticks(tick_marks, df_confusion.columns, rotation=45)
+    #plt.yticks(tick_marks, df_confusion.index)
+    plt.ylabel(df_confusion.index.name)
+    plt.xlabel(df_confusion.columns.name)
+
+    plt.show()
+
+def test_model(test_loader, PATH='trained_model.pth', num_classes=10):
+    # Testing loop
+    model = ComplexCNN(num_classes)
+    model.load_state_dict(torch.load(PATH))
+    model.eval()
+    correct = 0
+    total = 0
+    y_pred = []
+    y_actu = []
+    with torch.no_grad():
+        for batch in test_loader:
+            inputs, labels = batch[0], batch[1]
+            outputs = model(inputs)
+            predicted = outputs.argmax(1)
+            y_pred.append(predicted)  
+
+            total += labels.size(0)
+            labels = labels.argmax(1)
+            y_actu.append(labels) 
+            correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    print(f"Test Accuracy: {accuracy:.2f}%")
+
+    # Plotting confusion matrix
+    print('actual')
+    print(y_actu)
+    print(y_actu[0])
+    print(labels)
+    print('predicted')
+    print(y_pred)
+    print(y_pred[0])
+    print(predicted)
+    df_confusion = pd.crosstab(y_actu[0], y_pred[0])
+    plot_confusion_matrix(df_confusion)
+    
