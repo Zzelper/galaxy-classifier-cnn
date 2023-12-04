c347039fcf5805f3a1320fcd39ba331109104be0
Evan Chow <evan.sas.chow@gmail.com>
2023-12-04 18:14:02 -0500

    Added second convolutional layer

--- src/model.py
+++ src/model.py
@@ -12,17 +12,23 @@ class GalaxyCNN(nn.Module):
     def __init__(self, num_classes):
         super(GalaxyCNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 64, kernel_size=2, stride=2, padding=1)
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(4 * 16 * 64 * 64, 512)
+        self.dropout = nn.Dropout(p=0.1)
+        self.fc1 = nn.Linear(64*32*32, 512)
         self.fc2 = nn.Linear(512, num_classes)
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
-        x = self.conv1(x)
+        x = self.conv1(x) # [64,3,256,256] -> [64,16,256,256]
         x = self.relu(x)
-        x = self.pool(x)
-        x = x.view(-1, 16 * 128 * 128)
+        x = self.pool(x) # [64,16,256,256] -> [64,16,128,128]
+        x = self.conv2(x) # [64,16,128,128] -> [64, 64, 65,65]
+        x = self.relu(x)
+        x = self.pool(x) #[64,64, 32, 32]
+        x = self.dropout(x)
+        x = x.view(-1, 64*32*32)
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
