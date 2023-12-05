47b410aa8147bb8c2859521edd4a9414e5204e48
Evan Chow <evan.sas.chow@gmail.com>
2023-12-04 21:22:52 -0500

    Added method to evaluate accuracy

---
+++ src/results.py
@@ -0,0 +1,17 @@
+import numpy as np
+import torch
+
+
+def accuracy(model, loader):
+    correct = 0
+    total = 0
+    model.eval()
+    with torch.no_grad():
+        for batch in loader:
+            inputs, labels = batch["data"], batch["label"]
+            outputs = model(inputs)
+            predicted = outputs.argmax(1)
+            labels = labels.argmax(1)
+            correct += (predicted == labels).sum().item()
+            total += labels.size(0)
+    return correct/total
