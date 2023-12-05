d9e287d11934ed7ac2edc25ec3bbeef58295652c
Evan Chow <evan.sas.chow@gmail.com>
2023-12-05 03:01:52 -0500

    Added accuracy method and small changes to confusion matrix

--- src/results.py
+++ src/results.py
@@ -29,9 +29,9 @@ def make_confusion_matrix(test_loader, PATH='trained_model.pth', num_classes=10)
 
     with torch.no_grad():
         for batch in test_loader:
-            inputs, labels = batch[0], batch[1]
+            inputs, labels = batch["data"], batch["label"]
             outputs = model(inputs)
-            predicted = outputs.argmax(1)
+            _, predicted = torch.max(outputs.data, 1)
             labels = labels.argmax(1)
             expected = np.concatenate((expected, labels))
             test_labels = np.concatenate((test_labels, predicted))
@@ -46,3 +46,27 @@ def make_confusion_matrix(test_loader, PATH='trained_model.pth', num_classes=10)
     plt.savefig('ConfusionMatrix.png')
     plt.show()
     return
+
+def accuracy(model, loader, device):
+    '''
+    Function that loads and tests the model, then reports the accuracy
+
+    input:
+    - model: model to evaluate
+    - loader: test data
+    - device: device over which to run the model (gpu or cpu)
+
+    Output:
+    - accuracy: number of correct predictions / number of total predictions
+    '''
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch in loader:
+            inputs, labels = batch["data"].to(device), batch["label"].to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    return correct/total
